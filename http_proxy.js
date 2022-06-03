514a1fdb4d15cae52278f0c4a64c162cfb39d8e0
Your Name <you@example.com>
2022-06-03 23:05:22 +0800

    aa

--- http_proxy.js
+++ http_proxy.js
@@ -1,26 +1,26 @@
 import url from 'url';
 import http from 'http';
 import HttpsProxyAgent from 'https-proxy-agent';
+import axios from 'axios';
 
+async function getHttpProxy() {
+  let { data } = await axios.get("http://127.0.0.1:5555/random")
 
-http.get("http://127.0.0.1:5555/random", fucntion(res) {
-  console.log('"response" event!', res);
-})
+  return data;
+}
 
-// HTTP/HTTPS proxy to connect to
-var proxy = 'http://89.111.105.85:41258';
-console.log('using proxy server %j', proxy);
+async function sendRequest() {
+  const httpProxyUrl = await getHttpProxy()
+  console.log(i + ":" + httpProxyUrl)
+  let proxyHost = httpProxyUrl.split(':')[0]
+  let proxyPort = httpProxyUrl.split(':')[1]
+  let { data } = await axios.get("http://httpbin.org/get", {
+    proxy: {
+      host: proxyHost,
+      port: proxyPort
+    }
+  })
+  console.log(data)
+}
 
-// HTTPS endpoint for the proxy to connect to
-var endpoint = 'http://httpbin.org/get';
-console.log('attempting to GET %j', endpoint);
-var options = url.parse(endpoint);
-
-// create an instance of the `HttpsProxyAgent` class with the proxy server information
-var agent = new HttpsProxyAgent(proxy);
-options.agent = agent;
-
-http.get(options, function (res) {
-  console.log('"response" event!', res.headers);
-  res.pipe(process.stdout);
-});
+sendRequest()
