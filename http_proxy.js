0ec1d162d2f192676368f7902da25745df8f30cf
Your Name <you@example.com>
2022-06-03 23:19:14 +0800

    aa

--- http_proxy.js
+++ http_proxy.js
@@ -14,7 +14,7 @@ async function sendRequest() {
   //console.log(i + ":" + httpProxyUrl)
   let proxyHost = httpProxyUrl.split(':')[0]
   let proxyPort = httpProxyUrl.split(':')[1]
-  let { data } = await axios.get("http://httpbin.org/get", {
+  let { data } = await axios.get("http://wap3.jfsshi.com//api/v1/group/getmsg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9/eyJnaWQiOjg5LCJ1aWQiOjU3NzIsInZhbCI6Ijk2MDlkOTBhYjg5MTRiZDAwYzMyZmU1N2IwNTgxYTBhIiwiZXh0IjoxNjU1MDMyNTkzfQ/YfX-ljpCOgTfF3py0c4fhp7AVi_1HuKhZquaLBMKfoU&type=getmsg&data=null&index=null&reverse=false&count=40&_=1654267942989", {
     proxy: {
       host: proxyHost,
       port: proxyPort
@@ -24,4 +24,11 @@ async function sendRequest() {
   console.log(data)
 }
 
-sendRequest()
+while(true) {
+  try {
+    sendRequest()
+  } catch (error) {
+    
+  }
+}
+
